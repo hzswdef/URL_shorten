3ac38b3f1b041afb037f723f3bfc0d1bed5641d0
hzswdef <ostelia.group@gmail.com>
2021-12-20 22:50:15 +0200

    Create repo, add some files.

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <base href="/" />
+    <title>hzswdef | 404 Not Found</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/error.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+</head>
+<body>
+
+<div class="wrap">
+
+<div class="back-button" onclick="redirect_to('https://hzswdef.xyz/');">
+    <img src="assets/logout.png"/>
+
+    <span>
+        Back
+    </span>
+</div>
+
+<div class="error">
+    404
+    
+    <span>
+        Not Found.
+    </span>
+</div>
+
+</div>
+
+<script src="js/redirect.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <base href="/" />
+    <title>hzswdef | Social tab</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+</head>
+<body>
+
+<div class="wrap">
+
+<div id="loading-screen">
+    <svg class="spinner" viewBox="0 0 50 50">
+        <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+    </svg>
+</div>
+
+<div class="main-block">
+    <div class="profile">
+        yo
+    </div>
+</div>
+
+</div>
+
+<script src="js/jquery-3.6.0.js"></script>
+<script src="js/loading.js"></script>
+
+</body>
+</html>
