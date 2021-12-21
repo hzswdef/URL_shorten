4e2f3ab965a2458e17ac365ab2f5daeefd0f79db
hzswdef <ostelia.group@gmail.com>
2021-12-21 17:16:25 +0200

    Update db.sql data.

--- db.sql
+++ db.sql
@@ -1,5 +1,11 @@
 CREATE TABLE `shorten urls` (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     url CHAR(6),
-    point VARCHAR(256)
+    point VARCHAR(256),
+    token CHAR(16)
+)
+
+CREATE TABLE `tokens` (
+    token CHAR(16) PRIMARY KEY,
+    used INT(11) UNSIGNED
 )
