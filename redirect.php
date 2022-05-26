153c2c61690ef3f73e0cbdb302472eba99c9b52f
hzswdef <ostelia.group@gmail.com>
2022-05-26 03:07:11 +0300

    Refactoring, rename from check_url.php to redirect.php

---
+++ redirect.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once "lib/MySQL.class.php";
+
+class db_requests extends MySQL
+{
+    private $point;
+    
+    public function __construct(string $point)
+    {
+        parent::__construct();
+        $this->point = $point;
+    }
+    
+    protected function get_url(string $url)
+    {
+        return mysqli_fetch_array($this->conn->query("SELECT * FROM `shorten urls` WHERE `url`='$url'"));
+    }
+}
+
+class redirect extends db_requests
+{
+    private $point;
+    
+    public function __construct(string $point)
+    {
+        parent::__construct($point);
+        $this->point = $point;
+    }
+    
+    private function get_data()
+    {
+        $data = $this->get_url($this->point);
+        //var_dump($data);
+        self::close();
+        
+        return $data;
+    }
+    
+    // redirect to url
+    public function forward()
+    {
+        $data = $this->get_data();
+        
+        if ($data == null) {
+            die(include_once("404.php"));
+        }
+        
+        //
+        // MUST FIX IT
+        //
+        return header("Location: https://" . $data["point"]);
+    }
+}
+
+// Fetch unique code from requested URL
+$point = str_replace("/", "", $_SERVER['REQUEST_URI']);
+
+$redirect = new redirect($point);
+$redirect->forward();
+
+?>
